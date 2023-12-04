fe390a345f9f39cb3c8d500e2e8cdb8c328596dc
Muhammad Sharf Uddin <developersharf@gmail.com>
2023-12-04 15:52:20 +0600

    Update summation of 1 to N whose sum of digits is between-A-and-B_inclusive.c
    Given three numbers N, A, B. Print the summation of the numbers between 1
    and N whose sum of digits is between A and B inclusive.
    Sample Input                                                   Sample output
    20 2 5                                                                84
    10 1 2                                                                13
    
    
    Explanation:
    
    
    In the first simple:
    
    Among the numbers 1 to 20, the numbers whose sums of digits are between 2 and 5, are 2,3,4,5,11,12,13,14 and 20.
    So, the answer is: 84.

--- summation of 1 to N whose sum of digits is between-A-and-B_inclusive.c
+++ summation of 1 to N whose sum of digits is between-A-and-B_inclusive.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+int main(){
+
+    int n, a, b, sum1 = 0, s, sum2 = 0, i, j;
+    scanf("%d%d%d", &n, &a, &b);
+
+    for(i = 1; i <= n; i++){
+        sum1 = 0;
+        j = i;
+        while(j != 0){
+            s = j % 10;
+            sum1 = sum1 + s;
+            j = j / 10;            
+        }
+
+        if(sum1 >= a && sum1 <= b){
+            sum2 = sum2 + i;
+        }
+    }
+
+    printf("%d", sum2);
+
+    return 0;
+
+}
+
 
